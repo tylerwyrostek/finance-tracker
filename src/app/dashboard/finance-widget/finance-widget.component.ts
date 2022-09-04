f4b720b92f251f9d651b3407cd98638614751bc7
Tyler Wyrostek <tylerwyrostek@gmail.com>
2022-09-04 15:17:18 -0400

    planning data shows on dashboard

--- src/app/dashboard/finance-widget/finance-widget.component.ts
+++ src/app/dashboard/finance-widget/finance-widget.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ExpectedIncome } from 'src/app/planning/planning.types';
+import { PlannedFinances } from 'src/app/planning/planning.types';
 
 @Component({
   selector: 'app-finance-widget',
@@ -9,20 +9,37 @@ import { ExpectedIncome } from 'src/app/planning/planning.types';
 export class FinanceWidgetComponent implements OnInit {
   @Input() depositTotal: number = 0;
   @Input() withdrawalTotal: number = 0;
-  @Input() plannedIncome: ExpectedIncome[] = [];
+  @Input() plannedFinances!: PlannedFinances;
   plannedIncomeTotal: number = 0;
+  plannedExpenseTotal: number = 0;
   constructor() { }
 
   ngOnInit(): void {
     this.calculatePlannedIncome();
+    this.calculateExpenses();
   }
 
   private calculatePlannedIncome(): void{
-    this.plannedIncome.forEach(income =>{
+    this.plannedFinances.expectedIncome.forEach(income =>{
       this.plannedIncomeTotal  += income.Income;
     })
 
     console.log(this.plannedIncomeTotal);
+  }
+
+  private calculateExpenses(): void{
+    //needs
+    this.plannedFinances.needExpenses.forEach(need =>{
+      this.plannedExpenseTotal += need.expenseTotal;
+    })
+    //wants
+    this.plannedFinances.wantExpenses.forEach(want =>{
+      this.plannedExpenseTotal += want.expenseTotal;
+    })
+    //savings
+    this.plannedFinances.savingExpenses.forEach(saving =>{
+      this.plannedExpenseTotal += saving.expenseTotal;
+    })
   }
 
 }
